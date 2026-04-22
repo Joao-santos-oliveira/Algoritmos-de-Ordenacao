574617593bb07b3f9ddff50ab3fa3ea9602dcea1
joao <joaogabrielsantosoliveiraolive@gmail.com>
2026-04-21 23:50:03 -0300

    Implementação do algoritmo de ordenação Pigeonhole em C

--- metodosOrdenacao/C/pigeonhole/pigeonhole.c
+++ metodosOrdenacao/C/pigeonhole/pigeonhole.c
@@ -3,5 +3,31 @@
 #include "pigeonhole.h"
 
 void pigeonhole_sort(int* lista, int tamanho){
-    
+
+    if(tamanho <= 0) return;
+
+    int min = lista[0];
+    int max = lista[0];
+
+    for(int i = 1; i < tamanho; i++){
+        if(lista[i] < min) min = lista[i];
+        if(lista[i] > max) max = lista[i];
+    }
+
+    int range = max - min + 1;
+
+    int* holes = (int*) calloc(range, sizeof(int));
+
+    for(int i = 0; i < tamanho; i++){
+        holes[lista[i] - min]++;
+    }
+
+    int index = 0;
+    for(int i = 0; i < range; i++){
+        while(holes[i] > 0){
+            lista[index++] = i + min;
+            holes[i]--;
+        }
+    }
+    free(holes);
 }
