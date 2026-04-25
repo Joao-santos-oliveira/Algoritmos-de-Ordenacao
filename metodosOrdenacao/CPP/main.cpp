5ee3b8811f65ace7f3e87c0e8bfdb022aa24c1c7
alisson-h <ali123906123@gmail.com>
2026-04-25 19:55:52 -0300

    Criação Menu Interativo C++

--- metodosOrdenacao/CPP/main.cpp
+++ metodosOrdenacao/CPP/main.cpp
@@ -1,8 +1,77 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <functional>
+#include "problema.hpp"
+
+#include "radix/radix.hpp"
+#include "counting/counting.hpp"
+#include "introsort/introsort.hpp"
+
+// Altere para ESCOLHER_EM_EXECUCAO ou para os enums de Algoritmos
+#define ALGORITMO_USADO         ESCOLHER_EM_EXECUCAO
+// Altere para ESCOLHER_EM_EXECUCAO ou para um numero correspondente ao numero do input
+#define INPUT_USADO             ESCOLHER_EM_EXECUCAO
+
+// Altere para ESCOLHER_EM_EXECUCAO ou para um numero correspondente ao numero de excucoes
+// Serve para calcular o tempo médio do algoritmo
+#define QUANTIDADE_EXECUCOES    ESCOLHER_EM_EXECUCAO
+
+// 0 - Falso, 1 - Verdadeiro
+#define EXIBIR_INFORMAÇÕES      0
 
 using namespace std;
 
+void executar_algoritmo(problema* prob, function<void(vector<int>)> func_algoritmo){
+    vector<double> lista_tempos(prob->quantidade_execucoes);
+
+    for(int i = 0; i < prob->quantidade_execucoes; i++){
+        if (EXIBIR_INFORMAÇÕES) cout << "\nExecução " <<  i + 1 << " em andamento..." << endl;
+        
+        auto start = chrono::high_resolution_clock::now();
+        func_algoritmo(prob->lista);
+        auto end = chrono::high_resolution_clock::now();
+
+        auto duracao = chrono::duration_cast<chrono::seconds>(end - start);
+        lista_tempos[i] = duracao.count();
+        
+        if (EXIBIR_INFORMAÇÕES) cout << "Execução "<< i + 1 <<" concluída. Tempo gasto: "<< lista_tempos[i] << " segundos." << endl;
+    };
+
+    criar_output(prob, lista_tempos);
+}
+
+
 int main(){
-    
+    srand(42);
+
+    problema* problema = criar_problema(INPUT_USADO, ALGORITMO_USADO, QUANTIDADE_EXECUCOES);
+
+    switch (problema->algoritmo_usado) {
+        case RADIX_SORT: executar_algoritmo(problema, radix_sort); break;
+        case COUNTING_SORT: executar_algoritmo(problema, counting_sort); break;
+        case INTRO_SORT: executar_algoritmo(problema, intro_sort); break;
+        
+        case TODOS:
+            problema->algoritmo_usado = RADIX_SORT;
+            executar_algoritmo(problema, radix_sort);
+
+            problema->algoritmo_usado = COUNTING_SORT;
+            executar_algoritmo(problema, counting_sort);
+
+            problema->algoritmo_usado = INTRO_SORT;
+            executar_algoritmo(problema, intro_sort);
+        break;
+
+        default:
+            printf("Algoritmo não implementado.\n");
+        break;
+    }
+
+    printf("Algoritmos finalizados com sucesso...\n");
+    delete problema;
     return 0;
+
 }
