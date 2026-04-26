2a5fe2eb3adb757c0d2d439b2955073c5d11d3d9
alisson-h <ali123906123@gmail.com>
2026-04-26 00:10:56 -0300

    Implementação Radix Sorte C++

--- metodosOrdenacao/CPP/radix/radix.cpp
+++ metodosOrdenacao/CPP/radix/radix.cpp
@@ -1,8 +1,58 @@
 #include <iostream>
 #include <vector>
 
+#include "radix.hpp"
+#include "problema.hpp"
+
+#define IMPRIMIR_LISTA 0
+
 using namespace std;
 
-void radix_sort(vector<int> list){
-    for(int i = 0; i < 1000000; i++);
+int adquirir_maior_valor(vector<int> lista){
+    int maior = lista[0];
+    for(int valor : lista){
+        if (valor > maior) maior = valor;
+    }
+    return maior;
+}
+
+void counting_sort_simplificado(vector<int> &lista, vector<int> &lista_aux, int expo){
+    int count[10] = {0, 0, 0, 0, 0, 0 ,0, 0, 0, 0};
+    int digito = 0;
+    int tamanho = lista.size();
+
+    for (int i = 0; i < tamanho; i++){
+        count[(lista[i] / expo) % 10]++;
+    }
+    
+    for (int i = 1; i < 10; i++){
+        count[i] += count[i - 1];
+    }
+
+    for(int i = tamanho - 1; i >= 0; i--){
+        digito = (lista[i] / expo) % 10;
+        lista_aux[count[digito] - 1] = lista[i];
+        count[digito]--;
+    }
+
+    for(int i = 0; i < tamanho; i++){
+        lista[i] = lista_aux[i];
+    }
+}
+
+void radix_sort(vector<int> lista){
+    vector<int> lista_auxiliar(lista.size());
+
+    int maior_valor = adquirir_maior_valor(lista);
+    
+    int expo = 1;
+
+    if (IMPRIMIR_LISTA) exibir_lista(lista);
+
+    while (maior_valor / expo > 0){
+        counting_sort_simplificado(lista, lista_auxiliar, expo);
+        expo *= 10;
+    }
+
+    if (IMPRIMIR_LISTA) {cout << "Lista Organizada: " << endl; exibir_lista(lista);}
 }
--- metodosOrdenacao/CPP/radix/radix.hpp
+++ metodosOrdenacao/CPP/radix/radix.hpp
@@ -1,6 +1,16 @@
 #ifndef RADIX_H
 #define RADIX_H
+
+#include <iostream>
 #include <vector>
-void radix_sort(vector<int> listo);
+
+using namespace std;
+
+int adquirir_maior_valor(vector<int> lista);
+
+void counting_sort_simplificado(vector<int> &lista, vector<int> &lista_aux, int expo);
+
+void radix_sort(vector<int> lista);
+
 
 #endif /* RADIX_H */
