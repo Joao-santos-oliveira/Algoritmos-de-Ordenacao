93fd1e4b12e207f680515b06504a268714a86265
alisson-h <ali123906123@gmail.com>
2026-05-02 15:18:39 -0300

    Melhorando gerador de inputs

--- inputs/inputgenerator.py
+++ inputs/inputgenerator.py
@@ -1,26 +1,34 @@
+from tqdm import tqdm
 import random
 
 # Parametros para geração de inputs : -------
 entrada = 10 ** 7
 valor_minimo = 1
 valor_maximo = 10 ** 4
-arquivo_saida = 'inputs/input6.dat'
+arquivo_saida = 'inputs/input7.dat'
 
 random.seed(42)
 # -------------------------------------------
 
-def gerar_inputs(tamanho : int, min : int, max : int) -> list:
+
+def gerar_inputs(tamanho: int, min: int, max: int) -> list:
     inputs = []
-    for _ in range(tamanho):
+
+    # Barra de progresso na geração
+    for _ in tqdm(range(tamanho), desc="Gerando números", unit="num"):
         inputs.append(random.randint(min, max))
+
     return inputs
 
-def salvar_inputs(arquivo : str, numeros : list):
+
+def salvar_inputs(arquivo: str, numeros: list):
     with open(arquivo, 'w') as file:
         file.write(str(len(numeros)) + '\n\n')
-        file.write('\n'.join(map(str, numeros)))
+
+        # Barra de progresso na escrita
+        for numero in tqdm(numeros, desc="Salvando arquivo", unit="num"):
+            file.write(str(numero) + ',')
 
 
 numeros = gerar_inputs(entrada, valor_minimo, valor_maximo)
 salvar_inputs(arquivo_saida, numeros)
-
