eedf2940f3c3fe426f1f7e7088cac4aa9ff1ef9e
JPRS <joao.pedro.ribeiro.stos@gmail.com>
2026-05-02 16:33:23 -0300

    Criação do Counting Sort em C

--- metodosOrdenacao/C/counting/counting.c
+++ metodosOrdenacao/C/counting/counting.c
@@ -3,5 +3,39 @@
 #include "counting.h"
 
 void counting_sort(int* lista, int tamanho){
+
+    int maior_valor = adquirir_maior_valor_c(lista, tamanho);
+
+    int count[maior_valor+1];
+    for (int i = 0; i < maior_valor + 1; i++){
+        count[i] = 0;
+    }
+    for(int i = 0; i <tamanho; i++){
+        count[lista[i]]++;
+    }
+    for (int i = 1; i < maior_valor + 1; i++){
+        count[i] += count[i-1];
+    }
+
+    int aux[tamanho];
+
+    for(int i = tamanho-1; i > -1; i--){
+        int valor = lista[i];
+        aux[count[valor]-1] = valor;
+        count[valor]--;
+    }
+    for(int i = 0; i <tamanho; i++){
+        lista[i] = aux[i];
+    }
     
+
+}
+
+int adquirir_maior_valor_c(int* list, int tamanho){
+    int maior = list[0];
+    for (int i = 1; i < tamanho; i++){
+        if (list[i] > maior) maior = list[i];
+    }
+
+    return maior;
 }
--- metodosOrdenacao/C/counting/counting.h
+++ metodosOrdenacao/C/counting/counting.h
@@ -2,5 +2,6 @@
 #define COUNTING_H
 
 void counting_sort(int* lista, int tamanho);
+int adquirir_maior_valor_c(int* list, int tamanho);
 
 #endif /* COUNTING_H */
