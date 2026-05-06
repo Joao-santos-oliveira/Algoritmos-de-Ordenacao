e126abba9e40624037a81c033824681f50041fd4
alisson-h <ali123906123@gmail.com>
2026-05-06 11:46:19 -0300

    Implementacao Radix Sort GO

--- metodosOrdenacao/GO/radix/radix.go
+++ metodosOrdenacao/GO/radix/radix.go
@@ -1,5 +1,63 @@
 package radix
 
-func RadixSort(lista []int) {
+const (
+	IMPRIMIR_LISTA = false
+)
 
+func adquirir_maior_valor(lista []int) int {
+	maior := lista[0]
+	for i := 1; i < len(lista); i++ {
+		if lista[i] > maior {
+			maior = lista[i]
+		}
+	}
+
+	return maior
+}
+
+func counting_sort_simplificado(lista []int, lista_aux []int, exp int) {
+	count := make([]int, 10)
+	tam := len(lista)
+	for i := 0; i < tam; i++ {
+		count[(lista[i] / exp) % 10]++
+	}
+
+	for i := 1; i < 10; i++ {
+		count[i] += count[i-1]
+	}
+
+	for i := tam - 1; i >= 0; i-- {
+		digito := (lista[i] / exp) % 10
+		lista_aux[count[digito]-1] = lista[i]
+		count[digito]--
+	}
+
+	copy(lista, lista_aux)
+}
+
+// Radix Sort LSD
+func RadixSort(lista []int) {
+	lista_aux := make([]int, len(lista))
+
+	maior_valor := adquirir_maior_valor(lista)
+	exp := 1
+
+	if (IMPRIMIR_LISTA) {
+		println("Lista antes da ordenação:")
+		for v := range lista {
+			print(lista[v], ", ")
+		}
+	}
+
+	for exp = 1; maior_valor/exp > 0; exp *= 10 {
+		counting_sort_simplificado(lista, lista_aux, exp)
+	}
+
+	if (IMPRIMIR_LISTA) {
+		println("\nLista após a ordenação:")
+		for v := range lista {
+			print(lista[v], ", ")
+		}
+		println()
+	}
 }
